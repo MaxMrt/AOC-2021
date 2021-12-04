5a831ff0e7887652325de2d1d9e61b01a4c2fe02
Max Martens <max.martens@aol.de>
2021-12-04 02:27:59 +0100

    add option to select day

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const day = parseInt(process.argv[2], 10);
+if (Number.isNaN(day) || day < 1 || day > 25) {
+	console.error('You have to choose a day between 1 and 25');
+	process.exit(1);
+}
+
+const formattedDay: string = `day${day.toString().padStart(2, '0')}`;
+const file: string = `./${formattedDay}/index.ts`;
+const callback: string = require(file);
