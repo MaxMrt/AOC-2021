1310c7747ec7b4a8616132d5b8744dd149ed67bb
Max Martens <max.martens@aol.de>
2021-12-05 02:21:33 +0100

    prepare everything for Day2

---
+++ tests/day2test.ts
@@ -0,0 +1,14 @@
+// import {} from '../src/day02/index';
+
+// const testInput1 = `199
+// 200
+// 208
+// 210
+// 200
+// 207
+// 240
+// 269
+// 260
+// 263`;
+
+// test('day2 - first part', () => {});
