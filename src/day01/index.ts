73318c6d9e32d441693ccc344c41c22defde2019
Max Martens <max.martens@aol.de>
2021-12-05 02:09:59 +0100

    Complete Day 1

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -8,8 +8,21 @@ const data = fs.readFileSync('./assets/day1.txt', {
 const parse = (s: string) => s.split('\n').map((s) => parseInt(s));
 
 export function countIncreases(s: string) {
-	return parse(s).filter((value, index, array) => value > array[index - 1])
+	return parse(s).filter((value, index, number) => value > number[index - 1])
 		.length;
 }
 
-exports.first = countIncreases;
+export function sumMeasurements(s: string) {
+	let counter = 0;
+	let prevNumber = 0;
+	parse(s).forEach((value, index, number) => {
+		const sum = value + number[index + 1] + number[index + 2];
+		if (index === 0) prevNumber = sum;
+		if (sum > prevNumber) counter++;
+		prevNumber = sum;
+	});
+	return counter;
+}
+
+//console.log(countIncreases(data));
+//console.log(sumMeasurements(data));
--- tests/day1.test.ts
+++ tests/day1.test.ts
@@ -1,4 +1,4 @@
-import { countIncreases } from '../src/day01/index';
+import { countIncreases, sumMeasurements } from '../src/day01/index';
 
 const testInput1 = `199
 200
@@ -11,6 +11,21 @@ const testInput1 = `199
 260
 263`;
 
+const testInput2 = `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`;
+
 test('day1 - first part', () => {
 	expect(countIncreases(testInput1)).toBe(7);
+});
+
+test('day1 - second part', () => {
+	expect(sumMeasurements(testInput2)).toBe(5);
 });
