58b129bdc8467315a384cc89dc58599499311705
Max Martens <max.martens@aol.de>
2021-12-08 14:52:10 +0100

    add first test case for day3

---
+++ tests/day3.test.ts
@@ -0,0 +1,23 @@
+//import {} from '../src/day03/index';
+
+const testInput = `
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
+
+// test('day2 - first part', () => {
+// 	expect(function(testInput)).toBe(198);
+// });
+
+// test('day2 - second part', () => {
+// 	expect(function(testInput)).toBe(900);
+// });
